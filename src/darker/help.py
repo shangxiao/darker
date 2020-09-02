aa97bcd279a85ad3cf6b55156cc8e2075eb27335
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2020-09-02 23:00:47 +0300

    Further shorten command line parser building

--- src/darker/help.py
+++ src/darker/help.py
@@ -2,7 +2,7 @@
 
 ISORT_INSTRUCTION = "Please run `pip install 'darker[isort]'`"
 
-DESCRIPTION = [
+DESCRIPTION_PARTS = [
     "Re-format Python source files by using",
     "- `isort` to sort Python import definitions alphabetically within logical"
     " sections",
@@ -12,9 +12,10 @@ try:
     import isort
 except ImportError:
     isort = None
-    DESCRIPTION.extend(
+    DESCRIPTION_PARTS.extend(
         ["", f"{ISORT_INSTRUCTION} to enable sorting of import definitions"]
     )
+DESCRIPTION = "".join(DESCRIPTION_PARTS)
 
 
 SRC = "Path(s) to the Python source file(s) to reformat"
